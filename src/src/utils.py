ba7993659ced48c242e001161c48621bf5bcb21e
Alexey Egorov <egorovalexeyd@gmail.com>
2020-12-13 22:48:40 +0300

    Lesson 4HW white box

---
+++ src/src/utils.py
@@ -0,0 +1,40 @@
+def prefilter_items(data):
+    # Уберем самые популярные товары (их и так купят)
+    popularity = data.groupby('item_id')['user_id'].nunique().reset_index() / data['user_id'].nunique()
+    popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
+    
+    top_popular = popularity[popularity['share_unique_users'] > 0.5].item_id.tolist()
+    data = data[~data['item_id'].isin(top_popular)]
+    
+    # Уберем самые НЕ популярные товары (их и так НЕ купят)
+    top_notpopular = popularity[popularity['share_unique_users'] < 0.01].item_id.tolist()
+    data = data[~data['item_id'].isin(top_notpopular)]
+    
+    # Уберем товары, которые не продавались за последние 12 месяцев
+    
+    # Уберем не интересные для рекоммендаций категории (department)
+    
+    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб. 
+    
+    # Уберем слишком дорогие товарыs
+    
+    # ...
+    
+def postfilter_items(user_id, recommednations):
+    pass
+    
+    
+def get_similar_items_recommendation(data, user, model, itemid_to_id, N=5):
+    """Рекомендуем товары, похожие на топ-N купленных юзером товаров"""
+    
+    # your_code
+    
+    return res
+    
+    
+def get_similar_users_recommendation(user, model, N=5):
+    """Рекомендуем топ-N товаров, среди купленных похожими юзерами"""
+    
+    # your_code
+    
+    return res
