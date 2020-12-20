d151e13e200cbd4ee6cd065492d19dfd42d23b84
Alexey Egorov <egorovalexeyd@gmail.com>
2020-12-20 22:48:10 +0300

    Lesson 6 HW notebook preapred and lesson 4 HW updated

--- src/recomenders.py
+++ src/recomenders.py
@@ -38,7 +38,7 @@ class MainRecommender:
         # your_code. Это не обязательная часть. Но если вам удобно что-либо посчитать тут - можно это сделать
         
         self.user_item_matrix = self.prepare_matrix(data)  # pd.DataFrame
-        self.id_to_itemid, self.id_to_userid, self.itemid_to_id, self.userid_to_id = prepare_dicts(self.user_item_matrix)
+        self.id_to_itemid, self.id_to_userid, self.itemid_to_id, self.userid_to_id = self.prepare_dicts(self.user_item_matrix)
         
         if weighting:
             self.user_item_matrix = bm25_weight(self.user_item_matrix.T).T 
@@ -49,12 +49,19 @@ class MainRecommender:
     @staticmethod
     def prepare_matrix(data):
         
-        # your_code
+        user_item_matrix = pd.pivot_table(data,
+                                          index='user_id', columns='item_id',
+                                          values='quantity',  
+                                          aggfunc='count',
+                                          fill_value=0
+                                          )
+
+        user_item_matrix = user_item_matrix.astype(float)
         
         return user_item_matrix
     
-    @staticmethod
-    def prepare_dicts(user_item_matrix):
+    #@staticmethod
+    def prepare_dicts(self, user_item_matrix):
         """Подготавливает вспомогательные словари"""
         
         userids = user_item_matrix.index.values
@@ -84,13 +91,70 @@ class MainRecommender:
     def fit(user_item_matrix, n_factors=20, regularization=0.001, iterations=15, num_threads=4):
         """Обучает ALS"""
         
-        model = AlternatingLeastSquares(factors=factors, 
+        model = AlternatingLeastSquares(factors=n_factors, 
                                              regularization=regularization,
                                              iterations=iterations,  
                                              num_threads=num_threads)
-        model.fit(csr_matrix(self.user_item_matrix).T.tocsr())
+        model.fit(csr_matrix(user_item_matrix).T.tocsr())
         
         return model
+    
+    def _get_similar_item(self, item_id):
+        """Находит товар, похожий на item_id"""
+        recs = self.model.similar_items(self.itemid_to_id[item_id], N=2)  # Товар похож на себя -> рекомендуем 2 товара
+        top_rec = recs[1][0]  # И берем второй (не товар из аргумента метода)
+        return self.id_to_itemid[top_rec]
+
+    def _extend_with_top_popular(self, recommendations, N=5):
+        """Если кол-во рекоммендаций < N, то дополняем их топ-популярными"""
+
+        if len(recommendations) < N:
+            recommendations.extend(self.overall_top_purchases[:N])
+            recommendations = recommendations[:N]
+
+        return recommendations
+    
+    def _update_dict(self, user_id):
+        """Если появился новыю user / item, то нужно обновить словари"""
+
+        if user_id not in self.userid_to_id.keys():
+
+            max_id = max(list(self.userid_to_id.values()))
+            max_id += 1
+
+            self.userid_to_id.update({user_id: max_id})
+            self.id_to_userid.update({max_id: user_id})
+            
+    def _get_recommendations(self, user, model, N=5):
+        """Рекомендации через стардартные библиотеки implicit"""
+        
+        #print(self.itemid_to_id)
+
+        self._update_dict(user_id=user)
+        res = [self.id_to_itemid[rec[0]] for rec in model.recommend(userid=self.userid_to_id[user],
+                                        user_items=csr_matrix(self.user_item_matrix).tocsr(),
+                                        N=N,
+                                        filter_already_liked_items=False,
+                                        filter_items=[], #self.itemid_to_id[999999]],
+                                        recalculate_user=True)]
+
+        res = self._extend_with_top_popular(res, N=N)
+
+        assert len(res) == N, 'Количество рекомендаций != {}'.format(N)
+        return res
+
+    
+    def get_als_recommendations(self, user, N=5):
+        """Рекомендации через стардартные библиотеки implicit"""
+
+        self._update_dict(user_id=user)
+        return self._get_recommendations(user, model=self.model, N=N)
+
+    def get_own_recommendations(self, user, N=5):
+        """Рекомендуем товары среди тех, которые юзер уже купил"""
+
+        self._update_dict(user_id=user)
+        return self._get_recommendations(user, model=self.own_recommender, N=N)
 
     def get_similar_items_recommendation(self, user, N=5):
         """Рекомендуем товары, похожие на топ-N купленных юзером товаров"""
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-def prefilter_items(data):
+import pandas as pd
+import numpy as np
+
+def prefilter_items(data, take_n_popular=5000, item_features=None):
     # Уберем самые популярные товары (их и так купят)
     popularity = data.groupby('item_id')['user_id'].nunique().reset_index() / data['user_id'].nunique()
     popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
@@ -14,28 +17,28 @@ def prefilter_items(data):
     
     # Уберем не интересные для рекоммендаций категории (department)
     
+    if item_features is not None:
+        department_size = pd.DataFrame(item_features.groupby('department')['item_id'].nunique().sort_values(ascending=False)).reset_index()
+
+        department_size.columns = ['department', 'n_items']
+        rare_departments = department_size[department_size['n_items'] < 100].department.tolist()
+        items_in_rare_departments = item_features[item_features['department'].isin(rare_departments)].item_id.unique().tolist()
+
+        data = data[~data['item_id'].isin(items_in_rare_departments)]
+    
     # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб. 
     
+    #data['price'] = data['sales_value'] / (np.maximum(data['quantity'], 1))
+    #data = data[data['price'] > 5]
+    
     # Уберем слишком дорогие товарыs
-    data = data[data['price'] < 100]
+    #data = data[data['price'] < 100]
     
     # ...
+    
+    return data
     
 def postfilter_items(user_id, recommednations):
     pass
     
     
-def get_similar_items_recommendation(data, user, model, itemid_to_id, N=5):
-    """Рекомендуем товары, похожие на топ-N купленных юзером товаров"""
-    
-    # your_code
-    
-    return res
-    
-    
-def get_similar_users_recommendation(user, model, N=5):
-    """Рекомендуем топ-N товаров, среди купленных похожими юзерами"""
-    
-    # your_code
-    
-    return res
